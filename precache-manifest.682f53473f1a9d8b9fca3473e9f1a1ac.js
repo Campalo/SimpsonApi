d5b586ccb06071dedf6f6831b411111f8f7647d7
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-25 21:02:25 +0200

    Updates

---
+++ precache-manifest.682f53473f1a9d8b9fca3473e9f1a1ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfadac58cc1afb6df8e76825d8c71744",
+    "url": "/SimpsonApi/index.html"
+  },
+  {
+    "revision": "87a008e3dec8fa82271a",
+    "url": "/SimpsonApi/static/css/main.ee2b72ea.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/SimpsonApi/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "87a008e3dec8fa82271a",
+    "url": "/SimpsonApi/static/js/main.cdc46175.chunk.js"
+  },
+  {
+    "revision": "959bf49e5091c6cc4053",
+    "url": "/SimpsonApi/static/js/runtime~main.a76a13fd.js"
+  }
+]);
