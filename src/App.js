9b2c6cee4bc54a6b4b329ab471f92f88df6f2b5c
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-25 15:50:33 +0200

    update app.js with new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SelectSimpson from "./SelectSimpson";
+import DisplaySimpson from "./DisplaySimpson";
+
+const testSimpson = {
+  quote:
+    "Shoplifting is a victimless crime, like punching someone in the dark.",
+  character: "Nelson Muntz",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tell us your favourite Simpson character</h1>
+      <h2>We'll tell you his.er best quote!</h2>
+      <SelectSimpson />
+      <DisplaySimpson mySimpson={testSimpson} />
     </div>
   );
 }
